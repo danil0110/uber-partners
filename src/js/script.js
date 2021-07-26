116c9616a426f48c13586681684f6bf8e1bd7a3b
danil0110 <d.zalizchuk-2025@kpi.ua>
2021-07-26 15:04:01 +0300

    Formatted files

--- src/js/script.js
+++ src/js/script.js
@@ -1,38 +1,38 @@
-const menu = document.querySelector('.menu'),
-	hamburgerBtn = document.querySelector('.hamburger'),
-	menuLinks = menu.querySelectorAll('.menu_link');
+const menu = document.querySelector(".menu"),
+    hamburgerBtn = document.querySelector(".hamburger"),
+    menuLinks = menu.querySelectorAll(".menu_link");
 
 document.body.addEventListener(
-	'click',
-	e => {
-		if (e.target === hamburgerBtn || e.target.tagName === 'SPAN') {
-			return;
-		}
+    "click",
+    (e) => {
+        if (e.target === hamburgerBtn || e.target.tagName === "SPAN") {
+            return;
+        }
 
-		if (e.target !== menu) {
-			closeMenu();
-		}
-	},
-	true
+        if (e.target !== menu) {
+            closeMenu();
+        }
+    },
+    true
 );
 
-hamburgerBtn.addEventListener('click', toggleMenu);
+hamburgerBtn.addEventListener("click", toggleMenu);
 
-menuLinks.forEach(item => {
-	item.addEventListener('click', closeMenu);
+menuLinks.forEach((item) => {
+    item.addEventListener("click", closeMenu);
 });
 
 function toggleMenu() {
-	menu.classList.toggle('menu_active');
-	hamburgerBtn.classList.toggle('hamburger_active');
+    menu.classList.toggle("menu_active");
+    hamburgerBtn.classList.toggle("hamburger_active");
 }
 
 function openMenu() {
-	menu.classList.add('menu_active');
-	hamburgerBtn.classList.add('hamburger_active');
+    menu.classList.add("menu_active");
+    hamburgerBtn.classList.add("hamburger_active");
 }
 
 function closeMenu() {
-	menu.classList.remove('menu_active');
-	hamburgerBtn.classList.remove('hamburger_active');
+    menu.classList.remove("menu_active");
+    hamburgerBtn.classList.remove("hamburger_active");
 }
